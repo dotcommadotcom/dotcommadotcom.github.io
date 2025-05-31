e2b8baf01482f821bff80936706b8975b415159a
dotcommadotcom <byjiwoolee@gmail.com>
2025-05-31 16:41:11 -0500

    Basic pages

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const greetingElement = document.getElementById("greeting");
+    greetingElement.textContent = "This is Jiwoo";
+  });
+  
