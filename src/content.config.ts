e164efe6ea14292ee0939b5938044900ce934a9e
dotcommadotcom <byjiwoolee@gmail.com>
2025-06-01 17:25:43 -0500

    Add tags

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,14 +2,13 @@ import { glob } from 'astro/loaders';
 import { defineCollection, z } from 'astro:content';
 
 const posts = defineCollection({
-	// Load Markdown and MDX files in the `src/content/posts/` directory.
 	loader: glob({ base: './src/content/posts', pattern: '**/*.{md,mdx}' }),
-	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
 		pubDate: z.coerce.date(),
+        tags: z.array(z.string()),
+
 		updatedDate: z.coerce.date().optional(),
 		heroImage: z.string().optional(),
 	}),
